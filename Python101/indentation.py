e1b677a551cab5c789b3247a11bcf972442424c1
Andrii Sviatobatko <svyatobatko.a@gmail.com>
2022-03-08 13:01:08 +0200

    Section 1 Python is all about indentation and formatting

---
+++ Python101/indentation.py
@@ -0,0 +1,19 @@
+
+course = "Python 101"
+twitter = "@KalobTaulien"
+something = "something"
+somethingelse = "somethingelse"
+
+if course == "Python 101":
+    # Our code can go in here
+    if something:
+        # do something else
+        if somethingelse:
+            # do another thing
+            pass
+        # write more stuff in here
+    # Do more stuff in here
+
+if course == "Python 101":
+    # Our code can go in here
+    pass
