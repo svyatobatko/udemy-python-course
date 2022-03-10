3ba2854d63d6bf2b2ed78378756b447d798f60d6
Svyatobatko Andrey <svyatobatko.a@gmail.com>
2022-03-10 13:13:20 +0200

    Section 4 Comparison operator shortcuts Home work

--- Python101/comparison_shortcuts.py
+++ Python101/comparison_shortcuts.py
@@ -18,3 +18,45 @@ if something:
     print("This is true")
 else:
     print("This is false")
+
+# List with something = True
+something = [1, 2, 3]
+print("List")
+if something:
+    print("This is true")
+else:
+    print("This is false")
+# Empty list = False
+something = []
+if something:
+    print("This is true")
+else:
+    print("This is false")
+
+# Set with something = True
+something = {1, 2, 3}
+print("Set")
+if something:
+    print("This is true")
+else:
+    print("This is false")
+# Empty set = False
+something = {}
+if something:
+    print("This is true")
+else:
+    print("This is false")
+
+# Tuple with something = True
+something = (1, 2, 3,)
+print("Tuple")
+if something:
+    print("This is true")
+else:
+    print("This is false")
+# Empty tuple = False
+something = ()
+if something:
+    print("This is true")
+else:
+    print("This is false")
