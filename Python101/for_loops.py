4b1d65ee9cc5c726cb33cfac5844768e60a5a85d
Svyatobatko Andrey <svyatobatko.a@gmail.com>
2022-03-10 13:49:44 +0200

    Section 5 For loops Home work

--- Python101/for_loops.py
+++ Python101/for_loops.py
@@ -18,3 +18,17 @@ person = {
 }
 for key, value in person.items():
     print(f"The key is {key} and the value is {value}")
+
+print("We have next foods:")
+for food in fav_foods:
+    print(food)
+order = input("What food you want order?")
+ordered = False
+for food in fav_foods:
+    if food == order:
+        print(f"You ordered a {food}.")
+        ordered = True
+if ordered:
+    print("Your order will be ready fore a couple minutes. Pleas wait)")
+else:
+    print(f"Sorry, we don/'t have {order}. Pleas order something else.")
