df1dac4cfaa6cb1b08f0cc86b470ea11d13d5145
Svyatobatko Andrey <svyatobatko.a@gmail.com>
2022-03-10 16:01:52 +0200

    Section 8 The IN operator Home work

--- Python201/in_operator.py
+++ Python201/in_operator.py
@@ -15,3 +15,9 @@ person = {
 if key in person:
     print("Name is a valid dictionary key in the person object")
 
+all_set = {"Item 1", "Item 2", "Item 2", "Item 3"}
+selected_set = "Item 2"
+if selected_set in all_set:
+    print(f"Selected set is {selected_set}, present in all_set")
+else:
+    print(f"Selected set is {selected_set}, this value is absent in all_set")
