8c9366cc951a4c372322a5f5e328db42d13cbce4
Svyatobatko Andrey <svyatobatko.a@gmail.com>
2022-03-10 13:06:00 +0200

    Section 4 Comparison operator shortcuts

---
+++ Python101/comparison_shortcuts.py
@@ -0,0 +1,20 @@
+# None = False
+something = None
+if something:
+    print("This is true")
+else:
+    print("This is false")
+
+# Empty string = False
+something = ""
+if something:
+    print("This is true")
+else:
+    print("This is false")
+
+# String with something = True
+something = "something"
+if something:
+    print("This is true")
+else:
+    print("This is false")
