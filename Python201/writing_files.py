d7e8f96fac8470d59794a1df35a8297af7ab8e1f
Svyatobatko Andrey <svyatobatko.a@gmail.com>
2022-03-11 11:35:47 +0200

    Section 9 Writing files Homework

--- Python201/writing_files.py
+++ Python201/writing_files.py
@@ -1,3 +1,7 @@
 with open('writing_files.txt', 'w') as file:
     file.write("\nA second line!")
     file.write("\n\tThis is tabbed!")
+
+# Homework
+with open('writing_files.txt', 'a') as file:
+    file.write("\nAn appended line!")
