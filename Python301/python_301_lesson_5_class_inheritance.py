350a1d9213afc73cb390062782081e2e648a7d94
Svyatobatko Andrey <svyatobatko.a@gmail.com>
2022-03-13 19:10:34 +0200

    Section 17 OOP class inheritance Homework

--- Python301/python_301_lesson_5_class_inheritance.py
+++ Python301/python_301_lesson_5_class_inheritance.py
@@ -10,9 +10,11 @@ class Animal:
     def chase(self):
         pass
 
+
 class Tiger(Animal):
     def speak(self):
         print("They're GREEEEAATTTTTT")
+
 
 class HouseCat(Animal):
 
@@ -21,8 +23,20 @@ class HouseCat(Animal):
     def speak(self):
         print("Meow")
 
+
+class Dog(Animal):
+
+    fur_color = "White"
+
+    def speak(self):
+        print("Wow wow")
+
+
 tiger = Tiger()
 tiger.speak()
 cat = HouseCat()
 cat.speak()
 print(cat.fur_color)
+dog = Dog()
+dog.speak()
+print(dog.fur_color)
