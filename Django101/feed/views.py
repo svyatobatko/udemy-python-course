a548abacc48eb3562dd5e7576781262750c9b6fd
Svyatobatko Andrey <svyatobatko.a@gmail.com>
2022-03-21 13:51:25 +0200

    Section 23 Displaying dynamically added posts

--- Django101/feed/views.py
+++ Django101/feed/views.py
@@ -1,10 +1,12 @@
 from django.views.generic import TemplateView
 
+from .models import Post
+
 
 class HomePageView(TemplateView):
     template_name = "home.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['my_thing'] = "Hi all, this is dynamic!"
+        context['posts'] = Post.objects.all()
         return context
